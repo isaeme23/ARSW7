ba38871daac9eb6ac41f5003b032ab5988d5d98c
unknown <isabella.manrique@labinfo.is.escuelaing.edu.co>
2023-03-14 09:42:16 -0500

    Parte Back Logica

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,14 +1,45 @@
-var app = (function(){
+var app = (function(api){
+
+    var author;
+    var lista;
 
     function saveAuthor(){
-         author = $("#author").val();
-         alert(author);
-    };
+         $("#name").text(author + "'s Blueprints");
+    }
 
     function getBlueprintsAuthor(){
-        apimock.
-    };
+        author = $("#author").val();
+        api.getBlueprintsByAuthor(author, blueprints);
+    }
+
+    var blueprints = function(data){
+        $("#table tbody").empty();
+     if (data === undefined) {
+         alert("No existe el autor");
+         $("#name").empty();
+         $("#points").text("Total Points");
+         $("#nameblu").empty();
+     } else {
+        saveAuthor();
+         const datanew = data.map((elemento) => {
+             return {
+                 name: elemento.name,
+                 puntos: elemento.points.length
+             }
+         });
+
+         datanew.map((elementos) => {
+             $("#table > tbody:last").append($("<tr><td>" + elementos.name + "</td><td>" + elementos.puntos.toString() +
+                 "</td><td>" + "<button  id=" + elementos.name + " onclick=app.getBlueprintByAuthorAndName(this)>open</button>" + "</td>"));
+         });
+         const total = datanew.reduce((suma, {puntos}) => suma + puntos, 0);
+
+         $("#points").text("Total user points: " + total);
+        }
+    }
+
     return{
-        saveAuthor:saveAuthor
-    };
+        saveAuthor:saveAuthor,
+        getBlueprintsAuthor:getBlueprintsAuthor
+    }
 })(apimock);
